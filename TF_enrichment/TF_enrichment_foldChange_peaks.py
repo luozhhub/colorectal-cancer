59f425be0de9b58a0ebe9de3b992e0daa8c514ca
luozhihui <zhihl@ubuntu.com>
2019-11-14 20:42:45 +0800

    add script

---
+++ TF_enrichment/TF_enrichment_foldChange_peaks.py
@@ -0,0 +1,22 @@
+import os,sys
+
+
+def obtain_nfr_region():
+    nfr_dir = "/home/zhihl/Project/CRC/Chip_analysis/TF_fold_change/nfr_400_peaks"
+    weeks = ["2weeks", "4weeks", "7weeks", "10weeks"]
+    #markers = ["H3K27ac", "H3K4me1", "H3K4me3", "H3K27me3", "H3K9me2", "H3K9me3"]
+    #element = ["enhancer", "enhancer", "promoter", "repressed", "heterochromatin", "heterochromatin"]
+    markers = ["H3K27ac", "H3K4me3", "H3K27me3", "H3K9me3"]
+    element = ["enhancer", "promoter", "repressed",  "heterochromatin"]
+    element_dict = dict(zip(markers, element))
+    for we in weeks:
+        for mark in markers:
+            nfr_file = os.path.join(nfr_dir, "%s_%s_peaks.400.txt" % (we, mark))
+            fold_change_file = os.path.join("/home/zhihl/Project/CRC/Chip_analysis/TF_fold_change", "%s_%s_%s.bed" % (element_dict[mark], mark, we))
+            output_file = "/home/zhihl/Project/CRC/Chip_analysis/TF_fold_change/fold_change_nfr/%s_%s.400.bed" % (we, mark)
+            cmd = "bedtools intersect -a %s -b %s -wa > %s" % (nfr_file, fold_change_file, output_file)
+            os.system(cmd)
+
+
+if __name__ == "__main__":
+    obtain_nfr_region()
