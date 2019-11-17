f6d30380b389e2ace06d8fdccfd0e74cfbf3419f
luozhihui <luozhihui1123@163.com>
2019-11-17 14:24:38 +0800

    add script

--- TF_enrichment/TF_enrichment_foldChange_peaks.py
+++ TF_enrichment/TF_enrichment_foldChange_peaks.py
@@ -31,10 +31,24 @@ def transfer():
             cmd = "scp %s zhluo@211.69.141.147:/home/zhluo/zhao/TF_finding/" %(peak_file)
             os.system(cmd)
     
-    
-    
+def run_findMotif():    
+    weeks = ["2weeks", "4weeks", "7weeks", "10weeks"]
+    markers = ["H3K27ac", "H3K4me1", "H3K4me3", "H3K27me3", "H3K9me2", "H3K9me3"]
+    for we in weeks:
+        for mark in markers:
+            handle = open("/home/zhluo/Project/CRC/data_nazhang/pbs/%s_%s.pbs" %(we, mark), "w")
+            peak_file = "/home/zhluo/zhao/TF_finding/%s_%s.400.bed" % (we, mark)
+            output_dir = "/home/zhluo/Project/CRC/data_nazhang/step34_TF_enrichment/output_nfr/%s_%s" % (we, mark)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            handle.write("findMotifsGenome.pl %s mm10 %s" %(peak_file, output_dir))
+            handle.close()
 
 
 if __name__ == "__main__":
     #obtain_nfr_region()
-    transfer()
+    #transfer()
+    run_findMotif()
+    """
+     for i in `ls ./*.pbs` ;do qsub -q batch -V  $i ; done
+    """
